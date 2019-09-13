8f757f637c2d21d872fe8dd27b381682a1573133
wesleymutwiri <wesleymochiemo@gmail.com>
2019-09-13 21:51:43 +0300

    added app to settings

--- config/settings.py
+++ config/settings.py
@@ -34,13 +34,14 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'core',
+    'user',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
 ]
 
 MIDDLEWARE = [
