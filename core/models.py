547c622b9c779180d37cba48fa6eece403844428
wesleymutwiri <wesleymochiemo@gmail.com>
2019-09-13 22:50:54 +0300

    created a voter model

--- core/models.py
+++ core/models.py
@@ -105,3 +105,19 @@ class Role(models.Model):
             'person',
             'name'
         )
+
+
+class Vote(models.Model):
+    UP = 1 
+    DOWN = -1
+    VALUE_CHOICES = (
+        (UP, ""),
+        (DOWN, ""),
+    )
+    value = models.SmallIntegerField(choices=VALUE_CHOICES,)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE,)
+    voted_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('user', 'movie')
